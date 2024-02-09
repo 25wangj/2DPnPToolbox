a1179f13ac357c97dea9c29b83b6cc20e237acb6
Joshua Wang <25wangj@elmbrookstudents.org>
2024-02-09 11:04:35 -0600

    New initialization strategy described in v3 of paper

--- PoseTest.m
+++ PoseTest.m
@@ -12,7 +12,7 @@ for i=1:length(pts)
     points(2,:,:) = points(2,:,:) * 4 - 2;
     points(3,:,:) = points(3,:,:) * 4 + 4;
     homPts = pagemtimes(camMat, points);
-    camPts = homPts ./ homPts(3,:   ,:) + cat(1, 2 * randn(2, pts(i), tests), zeros(1, pts(i), tests));
+    camPts = homPts ./ homPts(3,:,:) + cat(1, 2 * randn(2, pts(i), tests), zeros(1, pts(i), tests));
     for j=1:tests
         rot = eye(3);
         while abs(rot(3,3)) > cos(pi/18)
@@ -38,6 +38,7 @@ hold on;
 for i=1:length(funcs)
     plot(pts, squeeze(data(1,i,:)), styles(i));
 end
+axis([0 200 0 0.1]);
 title("Translational Error");
 xlabel("Number of Points");
 ylabel("Average Translational Error (m)");
@@ -48,8 +49,9 @@ hold on;
 for i=1:length(funcs)
     plot(pts, squeeze(data(2,i,:)), styles(i))
 end
+axis([0 200 0 0.5]);
 title("Rotational Error");
 xlabel("Number of Points");
 ylabel("Average Rotational Error (deg)");
 pbaspect([1.5 1 1]);
-print("Plots/RotationPlot.eps", "-depsc2");
+print("Plots/Rotatio    nPlot.eps", "-depsc2");
